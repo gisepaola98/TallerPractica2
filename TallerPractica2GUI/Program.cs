09f839acda60864f3a15187f5f24bdc12303dba2
Gissell <gissellpmartinez@unicesar.edu.co>
2020-05-07 13:56:28 -0500

    tercer commit

--- TallerPractica2GUI/Program.cs
+++ TallerPractica2GUI/Program.cs
@@ -18,23 +18,7 @@ namespace TallerPractica2GUI
 
         static void Main()
         {
-            int Opcion;
-
-            do
-            {
-
-
-                Console.WriteLine("1. Registrar y liquidar");
-                Console.WriteLine("2. Consultar");
-                Console.WriteLine("3. Modificar");
-                Console.WriteLine("4. Eliminar");
-                Console.WriteLine("5. Salir");
-                Console.WriteLine("Por favor digite la opcion");
-                Opcion = Convert.ToInt32(Console.ReadLine());
-
-                switch (Opcion)
-                {
-                    case 1:
+            
                         Liquidacion liquidacion;
                         string NumeroLiquidacion, IdentificacionPaciente, TipoAfiliacion;
 
@@ -54,71 +38,31 @@ namespace TallerPractica2GUI
                         {
                             liquidacion = new RegimenSubsidiado();
                         }
-                        liquidacion.NumeroLiquidacion = NumeroLiquidacion;
-                        liquidacion.IdentificacionPaciente = IdentificacionPaciente;
-                        liquidacion.TipoAfiliacion = TipoAfiliacion;
+                           liquidacion.NumeroLiquidacion = NumeroLiquidacion;
+                           liquidacion.IdentificacionPaciente = IdentificacionPaciente;
+                           liquidacion.TipoAfiliacion = TipoAfiliacion;
 
-                        Console.WriteLine("Digite Valor del Servicio");
-                        liquidacion.ValorServicio = Convert.ToDecimal(Console.ReadLine());
+                           Console.WriteLine("Digite Valor del Servicio");
+                           liquidacion.ValorServicio = Convert.ToDecimal(Console.ReadLine());
+                           liquidacion.LiquidacionCuotaModerada();
+                           Console.WriteLine(liquidacionService.Guardar(liquidacion));
+            Console.Clear();
 
-                        liquidacion.LiquidacionCuotaModerada();
+            Console.WriteLine($"Numero de liquidacion :{liquidacion.NumeroLiquidacion}\n"
+                + $" Numero de identificacion:{liquidacion.IdentificacionPaciente}\n" +
+                $"Salario Devengado:{liquidacion.SalarioDevengado}\n" +
+                $" Tipo afiliacion :{liquidacion.TipoAfiliacion}\n" +
+                $"Valor del servicio : {liquidacion.ValorServicio}\n"
+                + $"Tarifa:{liquidacion.Tarifa}\n" +
+                $"Tope maximo:{liquidacion.TopeMaximo}\n"
+                + $"Couta Moderadora real:{liquidacion.CuotaReal}");
 
-                        Console.WriteLine(liquidacionService.Guardar(liquidacion));
-                        break;
-                    case 2:
+            Console.ReadKey();
 
-                        liquidaciones = liquidacionService.Consultar();
-                        foreach (Liquidacion item in liquidaciones)
-                        {
-                            Console.WriteLine($"Numero : {item.NumeroLiquidacion}");
-                            Console.WriteLine($"Identificacion: {item.IdentificacionPaciente}");
-                            Console.WriteLine($"Tipo De Afiliacion: {item.}");
-                            Console.WriteLine($"Salario Devengado: {item.SalarioDevengado}");
-                            Console.WriteLine($"Valor Del Servicio: {item.ValorServicio}");
-                            Console.WriteLine($"Tope: {item.TopeMaximo}");
-                            Console.WriteLine($"Cuota Moderada: {item.CuotaModerada}");
-                            Console.WriteLine($"Tarifa: {item.Tarifa}");
-
-                        }
-                        break;
-                    case 3:
-                        Console.WriteLine("Digite Numero de Liquidacion a Modificar: ");
-                        liquidacion = liquidacionService.Buscar(Console.ReadLine());
-                        if (liquidacion != null)
-                        {
-                            Console.WriteLine("Digite el nuevo Valor de Servicio: ");
-                            liquidacion.ValorServicio = Convert.ToDecimal(Console.ReadLine());
-                            liquidacion.LiquidacionCuotaModerada();
-                            liquidacionService.Modificar(liquidacion);
-                            Console.WriteLine("Modificado Correctamente...");
-                        }
-                        else
-                        {
-                            Console.WriteLine("No se encontro el numero d eliquidacion a modificar");
-                        }
-
-                        break;
-                    case 4:
-                        Console.WriteLine("Digite Numero de Liquidacion que desea Eliminar: ");
-                        liquidacion = liquidacionService.Buscar(Console.ReadLine());
-                        if (liquidacion != null)
-                        {
-                            //liquidacionService.Eliminar(liquidacion); Error
-                            Console.WriteLine("Opcion no terminada ");
-                            Console.ReadKey();
-                            break;
-                        }
-                        break;
-                    case 5:
-                        Console.WriteLine("Adios");
-                        break;
-                    default:
-                        Console.WriteLine("La opcion digitada no es valida");
-                        break;
-                }
-                Console.ReadKey();
-            } while (Opcion != 5);
 
         }
+
+
+
     }
 }
